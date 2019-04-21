4cb92adf4995c5a7fbd6f0c3fff10f70c0cd296d
A.I <tigrislover@sina.com>
2019-04-22 07:28:06 +0800

    spring cloud config客户端

---
+++ git-config-client/src/main/java/cn/origal/springcloud/GitConfigClientApplicaiton.java
@@ -0,0 +1,18 @@
+package cn.origal.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author  A.I.
+ * @date   2019/4/21 22:48
+ * @version   1.0
+ **/
+@SpringBootApplication
+public class GitConfigClientApplicaiton {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitConfigClientApplicaiton.class);
+    }
+}
