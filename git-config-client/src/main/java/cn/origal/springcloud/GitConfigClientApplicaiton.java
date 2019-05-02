e332f2a6d211d3f9b2dd70439e3854ce089db34f
A.I <tigrislover@sina.com>
2019-05-02 17:54:15 +0800

    配置高可用

--- git-config-client/src/main/java/cn/origal/springcloud/GitConfigClientApplicaiton.java
+++ git-config-client/src/main/java/cn/origal/springcloud/GitConfigClientApplicaiton.java
@@ -2,6 +2,7 @@ package cn.origal.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  *
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version   1.0
  **/
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GitConfigClientApplicaiton {
 
     public static void main(String[] args) {
